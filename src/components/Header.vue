2d27d1b61fada0b3ef75b4f95d66dd970d437b20
eomaianeto <eomaianeto@gmail.com>
2020-03-09 20:16:10 -0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+   <div id="header">
+
+     <header class="header">
+
+           <div class="img-header">
+             <img src="" alt="">
+           </div>
+           
+           <div class="nav-header">
+              <nav>
+                    <div class="router-links">
+                      <router-link to="/">Home</router-link>
+                      <router-link to="/about">Sobre Nós</router-link>
+                      <router-link to="/contact">Contato</router-link>
+                    </div>
+              </nav>
+
+
+           </div>
+     </header>
+     
+   </div>  
+</template>
+
+<script>
+export default {
+name:"Header"
+
+}
+</script>
+
+<style scoped>
+header {
+    width: 100vw;
+    height: 80px;
+}
+
+</style>
